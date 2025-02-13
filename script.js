e5d93e47121465f063069a39dd1aa613f3a3096f
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-13 12:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function CapitaliseName() {
+	let input = document.getElementById("fname");
+	input.value = input.value.toUpperCase();
+}
+
+// let input = querySelector("input");
+
+// input.addEventListener("blur",()=>{
+// 	console.log()
+// })
